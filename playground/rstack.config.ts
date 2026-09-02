fd2e972b34aa0020c3950bd3068b566efc188ca5
neverland <chenjiahan.jait@bytedance.com>
2026-09-02 22:20:35 +0800

    chore: migrate to Rstack CLI

--- playground/rstack.config.ts
+++ playground/rstack.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from '@rsbuild/core';
+// Configuration guide: https://rstack.rs/config
 import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginVue2 } from '@rsbuild/plugin-vue2';
-import { pluginVue2Jsx } from '../dist';
+import { define } from 'rstack';
+import { pluginVue2Jsx } from '../src/index.ts';
 
-export default defineConfig({
+define.app({
   plugins: [
     pluginVue2(),
     pluginVue2Jsx(),
